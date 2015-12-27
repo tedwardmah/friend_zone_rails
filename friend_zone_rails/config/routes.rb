52d026bfd845bb32c6c1f7937c7b2c86a952861e
Ted Mahoney <ted.mahoney22@gmail.com>
2015-12-27 16:25:18 -0500

    users, playlists, and songs tables created, html landing page lifted from spotify example

--- friend_zone_rails/config/routes.rb
+++ friend_zone_rails/config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+
+  resources :songs
+
+  resources :users
+
+  resources :playlists
+
+  get '/' => 'users#login_page'
+  post 'users/login' => 'users#login'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -22,7 +32,7 @@ Rails.application.routes.draw do
   #     end
   #
   #     collection do
-  #       get 'sold'  
+  #       get 'sold'
   #     end
   #   end
 
